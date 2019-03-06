693ea57344a6e8867af1e0b846c1e5eefd0ee0aa
Cotes <cotes.brandon@yahoo.com>
2019-03-06 10:34:51 -0500

    Updates

---
+++ precache-manifest.3ca73ff5751f907e1b1a967325c8c969.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "2925dfbcb4e44a6c492c",
+    "url": "/clicky-game/static/js/main.2925dfbc.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clicky-game/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "44b46f7d0a016dc258df94da87c2b10b",
+    "url": "/clicky-game/index.html"
+  }
+];
